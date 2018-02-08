286806d096c9a802905be26d051c011f5949ee4c
Geoff Simons <geoff.simons@gmail.com>
2018-02-07 22:13:28 -0800

    Updated importers for IPR and rosters

--- importers/import-ipr.js
+++ importers/import-ipr.js
@@ -3,10 +3,17 @@
 const info = JSON.parse(require('fs').readFileSync('data/playerinfo.json'));
 const { players } = info;
 
+// IPR.csv output
 // Object.keys(players).map(name => {
 //   const p = players[name];
 //   return [p.IPR, name].join(',');
 // })
 // .forEach(line => console.log(line));
 
-console.log(JSON.stringify(Object.keys(players), null, 2));
+// subs.json output
+// console.log(JSON.stringify(Object.keys(players), null, 2));
+
+// ifpa_num.csv output
+const { IFPA } = info;
+Object.keys(IFPA).map(num => [IFPA[num], num])
+  .forEach(row => console.log(row.join(',')));
