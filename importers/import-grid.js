6a2e89b66f884fafe5989ecfac7186ba58795b5e
Geoff Simons <geoff.simons@gmail.com>
2018-02-07 22:13:28 -0800

    Updated importers for IPR and rosters

---
+++ importers/import-grid.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('../lib/csv').load('data/season-9/roster-grid.csv')
+  .filter(row => row[3] != '')
+  .reduce((p, row) => {
+    p.push(row.slice(2,5));   // C-D-E
+    p.push(row.slice(7,10));  // H-I-J
+    p.push(row.slice(12,15)); // M-N-O
+    p.push(row.slice(17,20)); // Q-R-S
+    return p;
+  }, [])
+  .filter(row => row[0] != '')
+  .map(row => ([
+    row[0].trim().toLowerCase(),
+    ...row
+  ].join(',')))
+  .forEach(row => console.log(row));
