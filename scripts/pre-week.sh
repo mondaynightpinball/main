3e9b2d591db59bfeb46986b50d78f096430d0f66
Geoff Simons <geoff.simons@gmail.com>
2018-09-16 22:56:57 -0700

    Commands to update rosters and spawn matches

--- scripts/pre-week.sh
+++ scripts/pre-week.sh
@@ -1,1 +1,30 @@
-echo "Running Pre week:" `date` >> /home/mnp/main/data/cronlog/pre-week.log
+#!/bin/bash
+
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# TODO: It might be nice to get up to date on node version.
+nvm use 6
+
+LOG=/home/mnp/main/data/cronlog/pre-week.log
+
+echo "Running Pre week:" `date` >> $LOG
+
+cd /home/mnp/data-archive
+git checkout master
+git pull origin master
+
+# TODO Find a way to make the script not season hard coded.
+cp season-10/playerdb.csv ../main/data/season-10/playerdb.csv
+
+cd /home/mnp/main
+
+# TODO: Consider a git pull for main.
+
+# Refresh the season.json
+# TODO season 10 is hard coded in the import season script.
+node importers/import-season.js
+
+node util/spawn-matches.js
+
+sh scripts/restart.sh
