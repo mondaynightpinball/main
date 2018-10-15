62a4d8b1cb15baa524f986d7e1d6fda46216ed66
Geoff Simons <geoff.simons@gmail.com>
2018-10-14 19:23:46 -0700

    Opposing team removed from sugs

--- route/mnp.js
+++ route/mnp.js
@@ -412,7 +412,7 @@ function renderTeam(params) {
     canEdit: perms.canEdit,
     canRemove: perms.canRemove,
     canBegin: !team.ready && match.state == CONST.PREGAME && perms.canEdit && team.lineup.length > 7,
-    sugs: match.state == CONST.PREGAME ? JSON.stringify(getSuggestions([match.home.key, match.away.key]),null,2) : '[]'
+    sugs: match.state == CONST.PREGAME ? JSON.stringify(getSuggestions([team.key]),null,2) : '[]'
   },{
     head: head,
     content: template
