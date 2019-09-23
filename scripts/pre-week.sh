ef2dd6cb13da7e603b9e80e6ad99a9d5ec1a9839
Geoff Simons <geoff.simons@gmail.com>
2019-09-22 21:27:21 -0700

    No longer need to refresh rosters from github

--- scripts/pre-week.sh
+++ scripts/pre-week.sh
@@ -10,21 +10,9 @@ LOG=/home/mnp/main/data/cronlog/pre-week.log
 
 echo "Running Pre week:" `date` >> $LOG
 
-cd /home/mnp/data-archive
-git checkout master
-git pull origin master
-
-# TODO Find a way to make the script not season hard coded.
-cp season-12/playerdb.csv ../main/data/season-12/playerdb.csv
+# Roster updates are now done through the site, and 
+# will be archived in the post-week script.
 
 cd /home/mnp/main
-
-# TODO: Consider a git pull for main.
-
-# Refresh the season.json
-# TODO season 12 is hard coded in the import season script.
-node importers/import-season.js
-
 node util/spawn-matches.js
-
 sh scripts/restart.sh
